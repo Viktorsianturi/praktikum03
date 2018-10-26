d339a25ab40399f0410e2945613bc7e7a8d43555
Viktor-Sianturi <you@example.com>
2018-10-26 14:55:36 +0700

    menentukan jenis segitiga

---
+++ latihan3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cout <<"Berikan sisi A : ";
+    cin >> a;
+
+    cout <<"Berikan sisi B : ";
+    cin >> b;
+
+    cout <<"Berikan sisi C : ";
+    cin >> c;
+
+    if(a==b && a==c)
+    {
+        cout<<"Segitiga sama sisi"<<endl;
+    }
+    else if(a==b || a==c)
+    {
+        if(a!=b || a!=c)
+        {
+            cout<<"Segitiga sama kaki"<<endl;
+        }
+    }
+    else
+    cout<<"Segitiga sembarang"<<endl;
+
+    return 0;
+
+}
